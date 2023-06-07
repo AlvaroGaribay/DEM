f64877ec11e58f13bfdf20a05ae3b19faaa5ddef
AlvaroGaribay <95839872+AlvaroGaribay@users.noreply.github.com>
2023-06-07 11:11:36 -0600

    first commit

---
+++ src/app/tab1/tab1.page.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { Tab1Page } from './tab1.page';
+
+describe('TikPage', () => {
+  let component: Tab1Page;
+  let fixture: ComponentFixture<Tab1Page>;
+
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(Tab1Page);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
